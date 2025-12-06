8e5062ebe5556adcaa40b42586ed3bc642d4aa37
xviet <nguyenxuanviet03012003@gmail.com>
2025-12-06 16:57:07 +0700

    Update(doctor): Add Queies for Repo

---
+++ HospitalManagementSystem.Application/Features/Doctors/Queries/GetAllDoctor/GetAllDoctorQuery.cs
@@ -0,0 +1,15 @@
+﻿using HospitalManagementSystem.Application.DTOs.Doctors;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagementSystem.Application.Features.Doctors.Queries.GetAllDoctor
+{
+    public class GetAllDoctorQuery : IRequest<List<DoctorDto>>
+    {
+
+    }
+}
---
+++ HospitalManagementSystem.Application/Features/Doctors/Queries/GetAllDoctor/GetAllDoctorQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using HospitalManagementSystem.Application.DTOs.Doctors;
+using HospitalManagementSystem.Core.Interfaces.Repositories;
+using MediatR;
+using System.Threading;
+
+
+namespace HospitalManagementSystem.Application.Features.Doctors.Queries.GetAllDoctor
+{
+    public class GetAllDoctorQueryHandler : IRequestHandler<GetAllDoctorQuery, List<DoctorDto>>
+    {
+        private readonly IDoctorRepository _doctorRepository;
+        private readonly IMapper _mapper;
+        public GetAllDoctorQueryHandler(IDoctorRepository doctorRepository, IMapper mapper)
+        {
+            _doctorRepository = doctorRepository;
+            _mapper = mapper;
+        }
+        public async Task<List<DoctorDto>> Handle(GetAllDoctorQuery request, CancellationToken cancellationToken)
+        {
+            var doctors = await _doctorRepository.GetAllAsync();
+            return _mapper.Map<List<DoctorDto>>(doctors);
+        }
+
+    }
+}
